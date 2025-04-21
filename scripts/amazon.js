a74ffb46c00799f2eccfb50127a43ebede250a1b
Hamza <muhammadmhamza35@gmail.com>
2025-04-21 14:40:04 -0400

    created cart.js and updated cart by productID

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,12 +47,35 @@ products.forEach((product)=>{
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-ID="${product.productID}">
       Add to Cart
     </button>
   </div>`;
 })
 
-console.log(productsHTML);
-
 const grid = document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click', () => {
+    const productID = button.dataset.productID;
+
+    let matchingItem;
+
+    cart.forEach((item)=>{
+      if(productID===item.productID){
+        matchingItem=item;
+      } 
+    })
+
+    if(matchingItem){
+        matchingItem.quantity +=1;
+    } else {
+        cart.push({
+          productID: productID,
+          quantity: 1
+        });
+    }
+    console.log(cart)
+  })
+
+})
