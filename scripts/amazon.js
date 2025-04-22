4c85d0fdb5bd3c3e700eb87776cb75dd4872d599
Hamza <muhammadmhamza35@gmail.com>
2025-04-22 12:49:25 -0400

    turned products.js into a module and imported in amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
-let productsHTML = '';
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js"
 
+let productsHTML = '';
 
 products.forEach((product)=>{
   productsHTML += `
